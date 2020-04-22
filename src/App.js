35a50a25d7c73ee82e1ddff17de81108ec0a4ff8
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-22 17:25:37 +0200

    added axios

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import DisplaysMovies from './components/DisplaysMovies.jsx'
+
 
 function App() {
   return (
     <>
       <Header />
-      
+      <DisplaysMovies />      
       <Footer />
     </>
   );
---
+++ src/components/DisplaysMovies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const DisplaysMovies = () => {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default DisplaysMovies
